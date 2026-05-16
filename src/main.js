6af375e8a73809f484014c1f34438c095bb7d0aa
akrep17m-png <akr.ep17m@gmail.com>
2026-05-16 10:18:07 +0300

    add favicon and meta

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
-const isWebView = /TikTok|Instagram|FBAN|FBAV/i.test(navigator.userAgent);
-if (isWebView) {
+const ua = navigator.userAgent;
+
+const isTikTok = /musical_ly|BytedanceWebview|trill_/i.test(ua);
+
+if (isTikTok) {
   document.getElementById('webviewBanner').style.display = 'flex';
 }
+
 document.getElementById('webviewClose').addEventListener('click', () => {
   document.getElementById('webviewBanner').style.display = 'none';
 });
